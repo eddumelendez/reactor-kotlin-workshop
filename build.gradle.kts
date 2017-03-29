6bd8c8b3af89a750b2a3879e171998ecc3ef8d8d
Eddú Meléndez <eddu.melendez@gmail.com>
2017-03-29 14:00:53 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+buildscript {
+    val kotlinVersion = "1.1.1"
+    extra["kotlinVersion"] = kotlinVersion
+
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+apply {
+    plugin("kotlin")
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        setUrl("https://repo.spring.io/milestone")
+    }
+}
+
+val kotlinVersion = extra["kotlinVersion"] as String
+
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
+    compile("io.projectreactor:reactor-kotlin-extensions:1.0.0.M1")
+    testCompile("junit:junit:4.12")
+    testCompile("io.projectreactor.addons:reactor-test:3.0.5.RELEASE")
+}
