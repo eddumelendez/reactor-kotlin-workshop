86586df26ae781e800f485197915b6fd69f1ef48
Eddú Meléndez <eddu.melendez@gmail.com>
2017-03-30 16:07:08 -0500

    Add rxjava dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ val kotlinVersion = extra["kotlinVersion"] as String
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
     compile("io.projectreactor:reactor-kotlin-extensions:1.0.0.M1")
+    compile("io.reactivex.rxjava2:rxjava:2.0.5")
     testCompile("junit:junit:4.12")
     testCompile("io.projectreactor.addons:reactor-test:3.0.5.RELEASE")
 }
