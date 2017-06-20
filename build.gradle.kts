251d8d68af8b5be45f962d5a84bffe615e439525
Eddú Meléndez <eddu.melendez@gmail.com>
2017-06-19 19:15:41 -0500

    Upgrade dependencies
    * Upgrade kotlin version to 1.1.2-5
    * Upgrade reactor-kotlin version to 1.0.0.M2
    * Upgrade reactor-core and reactor-test to 3.1.0.M2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 buildscript {
-    val kotlinVersion = "1.1.1"
+    val kotlinVersion = "1.1.2-5"
     extra["kotlinVersion"] = kotlinVersion
 
     repositories {
@@ -25,13 +25,13 @@ val kotlinVersion = extra["kotlinVersion"] as String
 
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
-    compile("io.projectreactor:reactor-kotlin-extensions:1.0.0.M1") {
+    compile("io.projectreactor:reactor-kotlin-extensions:1.0.0.M2") {
         exclude("io.projectreactor", "reactor-core")
     }
-    compile("io.projectreactor:reactor-core:3.0.7.RELEASE")
+    compile("io.projectreactor:reactor-core:3.1.0.M2")
     compile("io.reactivex.rxjava2:rxjava:2.0.5")
     testCompile("junit:junit:4.12")
-    testCompile("io.projectreactor.addons:reactor-test:3.0.6.RELEASE") {
+    testCompile("io.projectreactor:reactor-test:3.1.0.M2") {
         exclude("io.projectreactor", "reactor-core")
     }
 }
