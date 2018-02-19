a091d5f6b872b554014b839644c88296866a9a29
Eddú Meléndez <eddu.melendez@gmail.com>
2018-02-19 18:01:14 -0500

    Upgrade reactor version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ tasks.withType<KotlinCompile> {
 
 dependencyManagement {
     imports {
-        mavenBom("io.projectreactor:reactor-bom:Bismuth-SR5")
+        mavenBom("io.projectreactor:reactor-bom:Bismuth-SR6")
     }
 }
 
