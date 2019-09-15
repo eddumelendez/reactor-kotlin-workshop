6c3f10d3f448c70279f881bcce137324d5cdaf65
Eddú Meléndez <eddu.melendez@gmail.com>
2019-09-14 22:52:18 -0500

    Upgrade reactor version to Californium-SR11

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ tasks.withType<KotlinCompile> {
 
 dependencyManagement {
     imports {
-        mavenBom("io.projectreactor:reactor-bom:Californium-SR5")
+        mavenBom("io.projectreactor:reactor-bom:Californium-SR11")
     }
 }
 
