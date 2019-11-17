8cb5b8d2d10801b9d8f5223af58d7da9a4f6051a
agusantoso <agusantoso@gmail.com>
2019-11-17 00:30:58 -0800

    update io.spring.dependency-management

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     val kotlinVersion = "1.3.50"
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
-    id("io.spring.dependency-management") version "1.0.7.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
 }
 
 repositories {
