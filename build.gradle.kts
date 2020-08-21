085b0593d68c35e8f1790d1530ca38550933a0b8
Eddú Meléndez <eddu.melendez@gmail.com>
2020-08-21 09:43:31 -0500

    Upgrade reactor version to Dysprosium-SR11

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ tasks.withType<KotlinCompile> {
 
 dependencyManagement {
     imports {
-        mavenBom("io.projectreactor:reactor-bom:Dysprosium-SR9")
+        mavenBom("io.projectreactor:reactor-bom:Dysprosium-SR11")
     }
 }
 
